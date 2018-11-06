2e31a50e9815e1f2d73184be52b0f552f37d6331
Gilbert <ktf2002kzz@gmail.com>
2018-11-06 12:08:59 +0900

    initial commit

--- baekjoon4673/main.c
+++ baekjoon4673/main.c
@@ -8,8 +8,26 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int dn(int n)
+{
+    int dn = n;
+    
+    while (n > 0)
+    {
+        dn += n % 10;
+        n = n/10;
+    }
+    return dn;
+}
+
+int arr[10000];
+int main(void)
+{
+    for(int n = 1; n <= 10000; n++)
+    {
+        arr[dn(n)] = 1;
+        if(!arr[n])
+            printf("%d \n", n); // 배열의 index 중 1이 아닌 값(0)이 저장되어 있는 index를 출력
+    }
     return 0;
 }
