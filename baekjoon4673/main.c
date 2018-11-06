ad8c3ad53dd9cab1d69ee4c1cdb816a3f531e006
Gilbert <ktf2002kzz@gmail.com>
2018-11-06 10:49:10 +0900

    Initial Commit

---
+++ baekjoon4673/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  baekjoon4673
+//
+//  Created by 이승섭 on 2018. 11. 6..
+//  Copyright © 2018년 이승섭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
